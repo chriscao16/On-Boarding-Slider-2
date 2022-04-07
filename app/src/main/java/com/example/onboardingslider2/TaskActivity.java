1b5b22d1becafb5db959a9bded0da4b50f94d487
chriscao16 <c.cao2157@gmail.com>
2022-04-06 22:03:35 -0700

    This is a test to: 1) Create a branch in git/github for the task page 2) Commiting/pushing changes to git/github

---
+++ app/src/main/java/com/example/onboardingslider2/TaskActivity.java
@@ -0,0 +1,16 @@
+package com.example.onboardingslider2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task);
+
+// This is a test for github
+    }
+}
